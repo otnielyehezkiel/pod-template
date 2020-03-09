43503bbdfe0769a120c42ac5ccf6cdd94c195f0f
Hendy Christianto <hendy.christianto@traveloka.com>
2020-03-09 18:33:50 +0700

    Fix scripting for swift project conf

--- setup/ConfigureSwift.rb
+++ setup/ConfigureSwift.rb
@@ -42,25 +42,13 @@ module Pod
 
       Pod::ProjectManipulator.new({
         :configurator => @configurator,
-        :xcodeproj_path => "templates/swift/Example/PROJECT.xcodeproj",
+        :xcodeproj_path => "templates/swift/PROJECT.xcodeproj",
         :platform => :ios,
         :remove_demo_project => (keep_demo == :no),
         :prefix => ""
       }).run
 
       `mv ./templates/swift/* ./`
-
-      # There has to be a single file in the Classes dir
-      # or a framework won't be created
-      `touch Pod/Classes/ReplaceMe.swift`
-
-      # The Podspec should be 8.0 instead of 7.0
-      text = File.read("NAME.podspec")
-      text.gsub!("7.0", "8.0")
-      File.open("NAME.podspec", "w") { |file| file.puts text }
-
-      # remove podspec for osx
-      `rm ./NAME-osx.podspec`
     end
   end
 
