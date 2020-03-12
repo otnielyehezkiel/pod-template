1de2533656af71c5aa10f072bab4af490a9f5f28
Hendy Christianto <hendy.christianto@traveloka.com>
2020-03-12 11:44:54 +0700

    Fix umbrella header and swift compatibility

---
+++ templates/ios/PROJECT/Dummy.swift
@@ -0,0 +1,9 @@
+//
+//  Dummy.swift
+//  PROJECT
+//
+//  Created by hendy.christianto on 12/03/20.
+//  Copyright © 2020 Traveloka. All rights reserved.
+//
+
+import Foundation
---
+++ templates/ios/Tests/Dummy.swift
@@ -0,0 +1,9 @@
+//
+//  Dummy.swift
+//  PROJECTTests
+//
+//  Created by hendy.christianto on 12/03/20.
+//  Copyright © 2020 Traveloka. All rights reserved.
+//
+
+import Foundation
