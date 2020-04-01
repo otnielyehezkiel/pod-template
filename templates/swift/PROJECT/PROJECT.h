b13572b2b712fd00e9cabc1b504974bb752d8d03
Hendy Christianto <hendy.christianto@traveloka.com>
2020-04-01 17:32:58 +0700

    Update public header explanation

--- templates/swift/PROJECT/PROJECT.h
+++ templates/swift/PROJECT/PROJECT.h
@@ -14,6 +14,6 @@ FOUNDATION_EXPORT double PROJECTVersionNumber;
 //! Project version string for PROJECT.
 FOUNDATION_EXPORT const unsigned char PROJECTVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <PROJECT/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import "PublicHeader.h"
 
 
