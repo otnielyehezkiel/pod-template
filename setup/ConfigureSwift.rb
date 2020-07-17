36f7f27c791c44512725b8a61c7e7583a1d7de28
Hendy Christianto <hendy.christianto@traveloka.com>
2020-07-17 16:07:48 +0700

    Use SandboxApp by default

--- setup/ConfigureSwift.rb
+++ setup/ConfigureSwift.rb
@@ -12,14 +12,10 @@ module Pod
     end
 
     def perform
-      keep_demo = configurator.ask_with_answers("Would you like to include a demo application with your library", ["Yes", "No"]).to_sym
-
-      if keep_demo == :yes
-        use_tvlapplication = configurator.ask_with_answers("Would you like use TVLApplication?", ["Yes", "No"]).to_sym
-        case use_tvlapplication
-          when :yes
-            configurator.add_pod_to_podfile "TVLApplication"
-        end
+      use_tvlapplication = configurator.ask_with_answers("Would you like use TVLApplication?", ["Yes", "No"]).to_sym
+      case use_tvlapplication
+        when :yes
+          configurator.add_pod_to_podfile "TVLApplication"
       end
 
       Pod::ProjectManipulator.new({
