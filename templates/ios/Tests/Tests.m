c4024d3760fbfec5af7cc6628af8a358e911a4d4
Hendy Christianto <hendy.christianto@traveloka.com>
2020-11-13 14:42:45 +0700

    Update Tests.m

--- templates/ios/Tests/Tests.m
+++ templates/ios/Tests/Tests.m
@@ -5,33 +5,3 @@
 //  Created by hendy.christianto on 06/03/20.
 //  Copyright © 2020 Traveloka. All rights reserved.
 //
-
-#import <XCTest/XCTest.h>
-
-@interface PROJECTTests : XCTestCase
-
-@end
-
-@implementation PROJECTTests
-
-- (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
-@end
